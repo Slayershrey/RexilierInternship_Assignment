8e9bf945b8741b19394004fa33b0798a0236c03b
Slayershrey <sawalkar336@gmail.com>
2024-06-21 09:00:27 +0530

    backend added

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const indexRoutes = require("./routes/index");
+const transactionRoutes = require("./routes/transactions");
+require("dotenv").config();
+var cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
+
+app.use("/api", indexRoutes);
+app.use("/api", transactionRoutes);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
